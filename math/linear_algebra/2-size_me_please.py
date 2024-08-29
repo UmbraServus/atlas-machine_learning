6649f054ed0590ea67797d155c2a961e2381a9c9
Kiko sprague <ksprague0592@gmail.com>
2024-08-29 18:50:57 +0000

    using numpy to get shape of matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import numpy as np
+""" module for calculating shape of matrix """
+
+def matrix_shape(matrix):
+    """ function for matrix shape calc """
+    array1 = np.array(matrix)
+    return array1.shape
