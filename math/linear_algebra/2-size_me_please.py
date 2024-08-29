9414b6a2ecfc7dcb85e1d2574aa957d12117da7e
Kiko sprague <ksprague0592@gmail.com>
2024-08-29 18:51:54 +0000

    using if elsee statements to get shape of matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
-import numpy as np
 """ module for calculating shape of matrix """
 
 def matrix_shape(matrix):
     """ function for matrix shape calc """
-    array1 = np.array(matrix)
-    return array1.shape
+
+    if not matrix:
+        return ()
+    
+    if isinstance(matrix[0], list):
+        if isinstance(matrix[0][0], list):
+            matrices = len(matrix)
+            rows = len(matrix[0])
+            columns = len(matrix[0][0])
+            return matrices, rows, columns
+        else:
+            rows = len(matrix)
+            columns = len(matrix[0]) if rows > 0 else 0
+            return rows, columns
+    else:
+        return len(matrix)
