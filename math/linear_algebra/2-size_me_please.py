b43626fb705d64e89e30bb070aa99531c06d3fb0
Kiko sprague <ksprague0592@gmail.com>
2024-08-30 01:54:31 +0000

    iterating thru the matrix as a stack and finding the length of each pop of the stack using isinstance to find make sure the type is a list.

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,19 +3,13 @@
 
 def matrix_shape(matrix):
     """ function for matrix shape calc """
+    shape = []
+    matrix_stack = [matrix]
 
-    if not matrix:
-        return ()
-    
-    if isinstance(matrix[0], list):
-        if isinstance(matrix[0][0], list):
-            matrices = len(matrix)
-            rows = len(matrix[0])
-            columns = len(matrix[0][0])
-            return matrices, rows, columns
-        else:
-            rows = len(matrix)
-            columns = len(matrix[0]) if rows > 0 else 0
-            return rows, columns
-    else:
-        return len(matrix)
+    while matrix_stack:
+        current_stack = matrix_stack.pop()
+        if isinstance(current_stack, list):
+            shape.append(len(current_stack))
+            if len(current_stack) > 0 and isinstance(current_stack[0], list):
+                matrix_stack.append(current_stack[0])
+    return shape
