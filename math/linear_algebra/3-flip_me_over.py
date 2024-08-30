10f888c77b4747987902936af8e93117d06e30e3
Kiko sprague <ksprague0592@gmail.com>
2024-08-30 04:00:44 +0000

    iterate over each col index, iterate over original row index append to new row and then append the new row to matrixT then  move to the next

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" module for transposing a 2d matrix """
+
+
+def matrix_transpose(matrix):
+    """ function for transposing a 2d matrix """
+    matrixT = []
+
+    for col_index in range(len(matrix[0])):
+        new_row = []
+        
+        for row_index in range(len(matrix)):
+            new_row.append(matrix[row_index][col_index])
+        matrixT.append(new_row)
+    return matrixT
