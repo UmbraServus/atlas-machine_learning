d96e04dd719d3cc6643005eb4840fddd9b1890b6
Kiko sprague <ksprague0592@gmail.com>
2024-09-03 15:43:16 +0000

    matrix mulitplication using 3 for loops and initializing mat3 with 0s instead of appending a complex mess of rows.

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" module for multiplying matrices together """
+
+
+def mat_mul(mat1, mat2):
+    """ method for multiplying matrices together """
+    m, n = len(mat1), len(mat1[0])
+    n2, p = len(mat2), len(mat2[0])
+    
+    if n != n2:
+        return None
+    
+    else:
+        mat3 = [[0 for _ in range(p)]for _ in range(m)]
+
+        for i in range(m):
+            for j in range(p):
+                for k in range(n):
+                    mat3[i][j] += mat1[i][k] * mat2[k][j]
+        return mat3
