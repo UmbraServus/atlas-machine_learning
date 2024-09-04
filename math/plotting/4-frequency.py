18b8e7d2dc116867606c951329d4efe30f0367be
Kiko sprague <ksprague0592@gmail.com>
2024-09-04 03:12:47 +0000

    okay added xticks to manually set the ticks and bins range 0 101 in 10s

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -7,11 +7,12 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
+    
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
-    plt.ylim(0, 30)
-    plt.xlim(0, 100)
     plt.title("Project A")
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor="black")
+    plt.ylim(0, 30)
+    plt.xlim(0, 100)
+    plt.xticks(np.arange(0, 101, 10))
     plt.show()
