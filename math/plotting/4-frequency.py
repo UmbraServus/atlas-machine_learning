099b39921a4e9ab9d3dca82440b6f0416c53d525
Kiko sprague <ksprague0592@gmail.com>
2024-09-04 16:16:11 +0000

    whitespace

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -9,7 +9,7 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-    
+
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
