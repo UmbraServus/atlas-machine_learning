9605c8b1d26761dc4f0c51b57ba8aedf6829c425
Kiko sprague <ksprague0592@gmail.com>
2024-09-10 22:18:55 +0000

    pmf instance method calculating the pmf

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -20,3 +20,22 @@ class Poisson():
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / len(data)
+
+            # Instance Methods
+    def pmf(self, k):
+        """ method for calc. p.m.f. (pmf = e**-lambtha * lambtha**k \ k!) 
+            
+            args:
+                k: number of successes """
+        if k < 0:
+            return 0
+        if not isinstance(k, int):
+            int(k)
+
+        e = 2.7182818285
+        exp_term = e ** -self.lambtha
+        lambtha_term = self.lambtha ** k
+        factorial_term = 1
+        for i in range(1, k + 1):
+            factorial_term *= i
+        return exp_term * lambtha_term / factorial_term
