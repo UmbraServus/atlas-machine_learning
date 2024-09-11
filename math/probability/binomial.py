1465cd6c3a8c404167afa6b804efbab1c1f5de14
Kiko sprague <ksprague0592@gmail.com>
2024-09-11 05:11:30 +0000

    okay I finally figured out how to get the right self.p and self.n have to calc total number of data points or trials and then take tha max data pt and multiply it by the n of data pts then take the sum of the data and divide it by this total trial to get self.p then self.n is max data pt divided by self.p and rounded. then recalc self.p which is number of sum of the data divided by number of trials

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -23,13 +23,8 @@ class Binomial():
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            total_trials = len(data)
+            total_trials = len(data) * max(data)
             successes = sum(data)
             self.p = successes / total_trials
-            self.n = round(total_trials)
-            self.p = float(self.p)
-            self.n = int(self.n)
-            if n < 0:
-                raise ValueError("n must be a positive value")
-            if p < 0 or p > 1:
-                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = round(max(data) / self.p)
+            self.p = successes / self.n
