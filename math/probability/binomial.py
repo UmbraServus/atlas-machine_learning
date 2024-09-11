d18731150980d9b72886c28dbe5bbdcfc684f8cc
Kiko sprague <ksprague0592@gmail.com>
2024-09-11 04:44:09 +0000

    binomial class begginings

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -1,2 +1,35 @@
 #!/usr/bin/env python3
+""" module for binomial distribution """
 
+
+class Binomial():
+    """ class for binomial """
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """ args:
+                data: list of data to be used for distribution
+                n: num of bernoulli trials
+                p: probability of success
+                """
+        if data is None:
+            if n < 0:
+                raise ValueError("n must be a positive value")
+            if p < 0 or p > 1:
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            total_trials = len(data)
+            successes = sum(data)
+            self.p = successes / total_trials
+            self.n = round(total_trials)
+            self.p = float(self.p)
+            self.n = int(self.n)
+            if n < 0:
+                raise ValueError("n must be a positive value")
+            if p < 0 or p > 1:
+                raise ValueError("p must be greater than 0 and less than 1")
