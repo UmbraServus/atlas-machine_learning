642d59c33a78673cd00dbf6335bb060285305ae5
Kiko sprague <ksprague0592@gmail.com>
2024-09-19 05:04:01 +0000

    wrong indention

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -28,22 +28,24 @@ class DeepNeuralNetwork():
                 input_size = nx
             else:
                 input_size = layers[l - 1]
-            self.__weights[f'W{l + 1}'] = (np.random.randn(layers[l], input_size)
-                                           * np.sqrt(2. / input_size))
+            self.__weights[f'W{l + 1}'] = (
+                np.random.randn(layers[l], input_size)
+                * np.sqrt(2. / input_size)
+                )
             self.__weights[f'b{l + 1}'] = np.zeros((layers[l], 1))
 
         # getters
-        @property
-        def L(self):
-            """getter for number of layers"""
-            return self.__L
+    @property
+    def L(self):
+        """getter for number of layers"""
+        return self.__L
 
-        @property
-        def cache(self):
-            """getter for cache"""
-            return self.__cache
+    @property
+    def cache(self):
+        """getter for cache"""
+        return self.__cache
 
-        @property
-        def weights(self):
-            """ getter for weights and bias"""
-            return self.__weights
+    @property
+    def weights(self):
+        """ getter for weights and bias"""
+        return self.__weights
