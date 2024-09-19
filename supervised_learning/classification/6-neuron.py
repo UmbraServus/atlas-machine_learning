a23897271ce068752f4de1f1c47763bdbec36d25
Kiko sprague <ksprague0592@gmail.com>
2024-09-19 02:44:20 +0000

    no way one character off in an exception?!

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -104,7 +104,7 @@ class Neuron():
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
         if iterations < 0:
-            raise ValueError("iteration must be a positive integer")
+            raise ValueError("iterations must be a positive integer")
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
         if alpha < 0:
