992e96fdf4fed4bc1eb83d617dd06f652d9a3447
Kiko sprague <ksprague0592@gmail.com>
2024-09-19 02:49:59 +0000

    pycode line too long

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -56,7 +56,8 @@ class Neuron():
                     m is the number of examples.
                 """
         m = Y.shape[1]
-        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = -1 / m * np.sum(Y * np.log(A) +
+                               (1 - Y) * np.log(1.0000001 - A))
         return cost
 
     def evaluate(self, X, Y):
