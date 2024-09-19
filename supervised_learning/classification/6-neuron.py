ce7f4e0a76389d5da9024b12e064ffc8e831f12d
Kiko sprague <ksprague0592@gmail.com>
2024-09-19 01:41:35 +0000

    hmm

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -56,8 +56,7 @@ class Neuron():
                     m is the number of examples.
                 """
         m = Y.shape[1]
-        cost = -1 / m * np.sum(Y * np.log(A) +
-                               (1 - Y) * np.log(1.0000001 - A))
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
 
     def evaluate(self, X, Y):
