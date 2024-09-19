eeffadec7cf8dff4587e1ab6e2b95ab933f9872f
Kiko sprague <ksprague0592@gmail.com>
2024-09-19 04:54:14 +0000

    added getters and private attributes

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -15,12 +15,12 @@ class DeepNeuralNetwork():
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list):
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
         for l in range(self.L):
             if not isinstance(layers[l], int) or layers[l] < 1:
                 raise TypeError("layers must be a list of positive integers")
@@ -28,6 +28,22 @@ class DeepNeuralNetwork():
                 input_size = nx
             else:
                 input_size = layers[l - 1]
-            self.weights[f'W{l + 1}'] = (np.random.randn(layers[l], input_size)
-                                     * np.sqrt(2. / input_size))
-            self.weights[f'b{l + 1}'] = np.zeros((layers[l], 1))
+            self.__weights[f'W{l + 1}'] = (np.random.randn(layers[l], input_size)
+                                           * np.sqrt(2. / input_size))
+            self.__weights[f'b{l + 1}'] = np.zeros((layers[l], 1))
+
+        # getters
+        @property
+        def L(self):
+            """getter for number of layers"""
+            return self.__L
+
+        @property
+        def cache(self):
+            """getter for cache"""
+            return self.__cache
+
+        @property
+        def weights(self):
+            """ getter for weights and bias"""
+            return self.__weights
