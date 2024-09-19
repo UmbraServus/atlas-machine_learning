849e037a1fa73af0894c780c3fd9d8c4abb16295
Kiko sprague <ksprague0592@gmail.com>
2024-09-19 02:41:54 +0000

    okay I see I had to assign threshold to the predictions after finding the cost with the return of forward_prop to avoid divide by zero

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -70,8 +70,8 @@ class Neuron():
                 Y: " " (1, m), contains the correct labels for the input data
                 """
         predictions = self.forward_prop(X)
+        cost = self.cost(Y, predictions)
         predictions = (predictions >= 0.5).astype(int)
-        cost = self.cost(Y, self.__A)
         return predictions, cost
 
     def gradient_descent(self, X, Y, A, alpha=0.05):
@@ -111,7 +111,7 @@ class Neuron():
             raise ValueError("alpha must be positive")
         
         # loop over iterations
-        for i in range(iterations + 1):
+        for i in range(iterations):
 
         # forward prop
             A = self.forward_prop(X)
