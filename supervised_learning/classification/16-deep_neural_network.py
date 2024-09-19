bcb5fd9af1e6a74c319732800737bc822555d599
Kiko sprague <ksprague0592@gmail.com>
2024-09-19 04:41:07 +0000

    fixed an issue with the bias where i forgot a set of parenthesis, also fixed the type error problem i didnt realize i had to check if the elements of the list were 0 but man i should of known.

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python3
-""" module doc """
+""" deep neural network module """
 import numpy as np
+
+
+class DeepNeuralNetwork():
+    """ DeepNeuralNetwork Class """
+
+    def __init__(self, nx, layers):
+        """ intialize nx and layers
+            nx: number of input features
+            layers: list representin the # of nodes in ea layer of the network
+            """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list):
+            raise TypeError("layers must be a list of positive integers")
+        for nodes in layers:
+            if nodes < 1:
+                raise TypeError("layers must be a list of positive integers")
+        
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for l in range(self.L):
+            if l == 0:
+                input_size = nx
+            else:
+                input_size = layers[l - 1]
+            self.weights[f'W{l}'] = (np.random.randn(layers[l], input_size)
+                                     * np.sqrt(2. / input_size))
+            self.weights[f'b{l}'] = np.zeros((layers[l], 1))
