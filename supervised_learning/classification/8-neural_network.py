e803b7b3ebf71b8e189945302226871f5e018738
Kiko sprague <ksprague0592@gmail.com>
2024-09-19 03:36:51 +0000

    start of neuralnetwork class initializing W1 b1 A1 W2 b2 and A2

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
 """ module doc """
 import numpy as np
+
+
+class NeuralNetwork():
+    def __init__(self, nx, nodes):
+        """ initialize nx and nodes
+            nx: number of input featues
+            nodes: number of nodes in the hidden layer
+            """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.normal(0, 1, (nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(0, 1, (1, nodes))
+        self.b2 = 0
+        self.A2 = 0
