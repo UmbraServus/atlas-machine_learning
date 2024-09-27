8cd1611bdd043d7a5ca376bb88d2f5bdedaae6b2
Kiko sprague <ksprague0592@gmail.com>
2024-09-27 02:56:04 +0000

    decodes the one hot using np argmax

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 """ module doc """
 import numpy as np
+
+
+def one_hot_decode(one_hot):
+  """ one hot decode converts a one hot matrix into a vector of labels
+    args:
+        one_hot: one-hot encoded ndarray with shape (classes, m)
+            classes: the maximum number of classes
+            m: the number of examples"""
+  return np.argmax(one_hot, axis=0)
