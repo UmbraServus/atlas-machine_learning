081fc65e2b0b3ef8d35e1c630846befad1be5fc0
Kiko sprague <ksprague0592@gmail.com>
2024-09-27 03:03:34 +0000

    added validation for onehot if its not a ndarray or the axis at 0 is < 2

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -4,9 +4,11 @@ import numpy as np
 
 
 def one_hot_decode(one_hot):
-  """ one hot decode converts a one hot matrix into a vector of labels
-    args:
+    """ one hot decode converts a one hot matrix into a vector of labels
+      args:
         one_hot: one-hot encoded ndarray with shape (classes, m)
             classes: the maximum number of classes
             m: the number of examples"""
-  return np.argmax(one_hot, axis=0)
+    if not isinstance(one_hot, np.ndarray) or one_hot.shape[0] < 2:
+        return None
+    return np.argmax(one_hot, axis=0)
