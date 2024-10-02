bba06671106259b135cb0b99379a5bae675028a3
UmbraServus <ksprague0592@gmail.com>
2024-10-02 15:10:35 -0500

    calculating loss with tf.losses.softmax_cross_entropy

---
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module for calculating accuracy of the model"""
+import tensorflow.compat.v1 as tf
+
+
+def calculate_loss(y, y_pred): 
+    """ that calculates the softmax cross-entropy loss of a prediction:
+    args:
+        y: a placeholder for the labels of the input data
+        y_pred: a tensor containing the networks predictions
+    Returns: a tensor containing the loss of the prediction """
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=y,
+        logits=y_pred
+    )
+    return loss
