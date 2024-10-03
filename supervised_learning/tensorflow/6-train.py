553422b17fb6d8fec8763cfdbda1b2eeae68b279
UmbraServus <ksprague0592@gmail.com>
2024-10-02 21:36:40 -0500

    fixed extra space in train

---
+++ supervised_learning/tensorflow/6-train.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import numpy as np
+
+# Assuming these functions are defined and imported correctly
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
+    """
+    Builds, trains, and saves a neural network classifier.
+
+    Args:
+    X_train: numpy.ndarray containing the training input data
+    Y_train: numpy.ndarray containing the training labels
+    X_valid: numpy.ndarray containing the validation input data
+    Y_valid: numpy.ndarray containing the validation labels
+    layer_sizes: list containing the number of nodes in each layer of the network
+    activations: list containing the activation functions for each layer of the network
+    alpha: learning rate
+    iterations: number of iterations to train over
+    save_path: path to save the model
+
+    Returns:
+    The path where the model was saved.
+    """
+    
