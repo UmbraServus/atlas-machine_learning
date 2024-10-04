e046126d4940eeafded74a16f50809191e059e7e
UmbraServus <ksprague0592@gmail.com>
2024-10-03 21:36:34 -0500

    spent forever figuring out train.py ! had missing imports and v1 import wasnt working right but I found a few work arounds also had to comprehend how to set it up and in which order while also iterating and passing the right ops to the session to run in the graph.

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 import numpy as np
-
-# Assuming these functions are defined and imported correctly
+tf.compat.v1.disable_eager_execution()
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+forward_prop = __import__('2-forward_prop').forward_prop
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
-create_placeholders = __import__('0-create_placeholders').create_placeholders
 create_train_op = __import__('5-create_train_op').create_train_op
-forward_prop = __import__('2-forward_prop').forward_prop
+
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """
@@ -26,4 +26,34 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
     Returns:
     The path where the model was saved.
     """
+    graph = tf.Graph()
+    with graph.as_default():
+        x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+        y_pred = forward_prop(x, layer_sizes, activations)
+        loss = calculate_loss(y, y_pred)
+        accuracy = calculate_accuracy(y, y_pred)
+        train_op = create_train_op(loss, alpha)
+
+    with tf.compat.v1.Session(graph=graph) as sess:
+        sess.run(tf.compat.v1.global_variables_initializer())
     
+        for i in range(iterations + 1):
+            if i > 0:
+                _, cost = sess.run([train_op, loss], feed_dict={x: X_train, y: Y_train})
+            else:
+                training_cost = sess.run(loss, feed_dict={x: X_train, y: Y_train})
+        
+            training_accuracy = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
+            validation_cost = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
+            validation_accuracy = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+
+            if i % 100 == 0 or i == iterations:
+                print(f"After {i} iterations:")
+                print(f"\tTraining Cost: {training_cost}")
+                print(f"\tTraining Accuracy: {training_accuracy}")
+                print(f"\tValidation Cost: {validation_cost}")
+                print(f"\tValidation Accuracy: {validation_accuracy}")
+    
+        saver = tf.compat.v1.train.Saver()
+        save_path = saver.save(sess, save_path)
+    return save_path
