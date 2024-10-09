0c81915b1a0e89cd87dbeba6c943f381de51d208
UmbraServus <ksprague0592@gmail.com>
2024-10-09 14:40:43 -0500

    apparently using permutation directly on x and y doesnt work and you must use it on an i or indices variable to consistently apply it across both matrices.

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -12,6 +12,7 @@ def shuffle_data(X, Y):
             m is the same number of data points as in X
             ny is the number of features in Y
     Returns: the shuffled X and Y matrices """
-    X = np.random.permutation(X)
-    Y = np.random.permutation(Y)
+    i = np.random.permutation(X.shape[0])
+    X = X[i]
+    Y = Y[i]
     return X, Y
