ac570d1f6f7652fa2f8c007664566d03a2e23dbc
UmbraServus <ksprague0592@gmail.com>
2024-10-09 14:28:08 -0500

    method to shuffle data.

---
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""module for shuffling data in matrices the same way"""
+import numpy as np
+
+def shuffle_data(X, Y):
+    """ shuffles the data points in two matrices the same way
+    args:
+        X: the first numpy.ndarray of shape (m, nx) to shuffle
+            m is the number of data points
+            nx is the number of features in X
+        Y: the second numpy.ndarray of shape (m, ny) to shuffle
+            m is the same number of data points as in X
+            ny is the number of features in Y
+    Returns: the shuffled X and Y matrices """
+    X = np.random.permutation(X)
+    Y = np.random.permutation(Y)
+    return X, Y
