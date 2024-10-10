e92775658be607fa921a62a724ecf7674ec9d11d
UmbraServus <ksprague0592@gmail.com>
2024-10-09 21:50:16 -0500

    grad descent w/ momentum optimization algorithm. updates teh velocity/new moment and variable

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,18 @@
+#1/usr/bin/env python3
+"""module for updating a variable using grad. descent w// momentum opti alg."""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """ updts a var. usin the gradient descent w momentum opti. algorithm
+    v = β1 * v + (1 − β1) * ∇J(θ)
+    var = var - alpha * v
+    args:
+        alpha is the learning rate
+        beta1 is the momentum weight
+        var is a numpy.ndarray containing the variable to be updated
+        grad is a numpy.ndarray containing the gradient of var
+        v is the previous first moment of var
+    Returns: the updated variable and the new moment, respectively"""
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+    return var, v
