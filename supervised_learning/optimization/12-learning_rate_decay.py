3e3989c2bed3c856eaf9cb48facd81ece7c62fc0
UmbraServus <ksprague0592@gmail.com>
2024-10-09 23:52:27 -0500

    line 12 too long and line 7 too long

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -4,11 +4,12 @@ import tensorflow as tf
 
 
 def learning_rate_decay(alpha, decay_rate, decay_step):
-    """ that creates a learning rate decay operation in tensorflow using inverse time decay
+    """ creates a learning rate decay operation in tensorflow 
+    using inverse time decay
         the learning rate decay should occur in a stepwise fashion
     args:
         alpha is the original learning rate
-        decay_rate: weight used to determine the rate at which alpha will decay
+        decay_rate: w8 used to determine the rate at which alpha will decay
         decay_step: number of passes of grad descent that should occur before
         alpha is decayed further
     Returns: the learning rate decay operation """
