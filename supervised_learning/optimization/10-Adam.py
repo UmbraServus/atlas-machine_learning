fac598d6811c603ad2008bcabd9b7b9a52613213
UmbraServus <ksprague0592@gmail.com>
2024-10-10 22:47:54 -0500

    adam using tensorflow

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -11,3 +11,10 @@ def create_Adam_op(alpha, beta1, beta2, epsilon):
         beta2: the weight used for the second moment
         epsilon: a small number to avoid division by zero
     Returns: optimizer"""
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon
+        )
+    return optimizer
