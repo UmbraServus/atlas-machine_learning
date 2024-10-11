bc20a028b598090a1a7e8d7fcdbcb33909ef28ae
UmbraServus <ksprague0592@gmail.com>
2024-10-10 23:17:17 -0500

    using tensorflow for the inversetimedecay I guess it wanted staircase=True to floor the decaysteps/decayrate

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -13,3 +13,10 @@ def learning_rate_decay(alpha, decay_rate, decay_step):
         decay_step: number of passes of grad descent that should occur before
         alpha is decayed further
     Returns: the learning rate decay operation """
+    optimizer = tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True
+    )
+    return optimizer
