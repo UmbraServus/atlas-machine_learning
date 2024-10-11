5b11de0c1f352a72929dcf5aa4c717c696875438
UmbraServus <ksprague0592@gmail.com>
2024-10-10 22:28:35 -0500

    adam optimization

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -16,3 +16,9 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
         s: the previous second moment of var
         t: the time step used for bias correction
     Returns: updated var, new 1st moment, and new 2nd moment, respectively"""
+    v = beta1 * v + (1 - beta1) * grad
+    s = beta2 * s + (1 - beta2) * grad ** 2
+    v_hat = v / (1 - beta1 ** t)
+    s_hat = s / (1 - beta2 ** t)
+    var = var - (alpha * v_hat) / (np.sqrt(s_hat) + epsilon)
+    return var, v, s
