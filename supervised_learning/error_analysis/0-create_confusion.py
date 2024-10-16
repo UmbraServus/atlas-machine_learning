71ebc91923549bf657f449c3ffc6764eda0a9858
UmbraServus <ksprague0592@gmail.com>
2024-10-15 22:25:44 -0500

    pycode style 12 and 9 too long also missing space after comma in line 21 after confusion_mat.

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -6,10 +6,10 @@ import numpy as np
 def create_confusion_matrix(labels, logits):
     """ creates a confusion matrix
     args:
-        labels: 1-hot np.ndarr shape (m, classes) containing the correct labels
+        labels: 1-hot np.ndarr shape (m, classes) containing correct labels
             m is the number of data points
             classes is the number of classes
-        logits: 1-hot np.ndarr shape (m, classes) containing the predicted labels
+        logits: 1-hot np.ndarr shape (m, classes) containing predicted labels
 Returns: a confusion np.ndarray of shape (classes, classes) w/ row idxs
     representing true labels & col idxs representing the pred. labels"""
 
@@ -18,5 +18,5 @@ Returns: a confusion np.ndarray of shape (classes, classes) w/ row idxs
     y_pred = np.argmax(logits, axis=1)
     confusion_mat = np.zeros((classes, classes), dtype=float)
 
-    np.add.at(confusion_mat,(y_true, y_pred), 1)
+    np.add.at(confusion_mat, (y_true, y_pred), 1)
     return confusion_mat
