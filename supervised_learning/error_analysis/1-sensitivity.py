6cc16ca80884ccb7877439860829c83a9417f7b6
UmbraServus <ksprague0592@gmail.com>
2024-10-16 01:19:27 -0500

    Sensitivity = TP / TP + FN originally had a for loop that worked but I felt like there was another way so I asked chatgpt if there was a way to add the diagonal indices of a matrix and bam there is.

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3  
+""" sensitivity module """
+import numpy as np
+
+
+def sensitivity(confusion): 
+    """that calculates the sensitivity for each class in a confusion matrix
+        Sensitivity = TP / TP + FN
+    args:
+        confusion: np.ndarr shape (classes, classes) row idxs represent the 
+            corr labels & col idxs repre the pred labels
+            classes is the number of classes
+    Returns: np.ndarray shape (classes,) containing sensitivity of ea class"""
+    sensitivity = np.zeros(len(confusion))
+    TP = np.diag(confusion)
+    FN = np.sum(confusion, axis=1) - TP
+    sensitivity = TP / (FN + TP)
+    return sensitivity
