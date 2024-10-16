22eb78130c048345c0eacf1a31e21870b2b061e9
UmbraServus <ksprague0592@gmail.com>
2024-10-15 22:18:19 -0500

    using np.add to add one at each indx of y_true, y_pred in the confusion matrix to complete the confusion matrix.

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Confusion Matrix Module """
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """ creates a confusion matrix
+    args:
+        labels: 1-hot np.ndarr shape (m, classes) containing the correct labels
+            m is the number of data points
+            classes is the number of classes
+        logits: 1-hot np.ndarr shape (m, classes) containing the predicted labels
+Returns: a confusion np.ndarray of shape (classes, classes) w/ row idxs
+    representing true labels & col idxs representing the pred. labels"""
+
+    classes = logits.shape[1]
+    y_true = np.argmax(labels, axis=1)
+    y_pred = np.argmax(logits, axis=1)
+    confusion_mat = np.zeros((classes, classes), dtype=int)
+
+    np.add.at(confusion_mat,(y_true, y_pred), 1)
+    return confusion_mat
