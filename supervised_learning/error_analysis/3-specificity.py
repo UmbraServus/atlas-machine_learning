fae368bf488af2c574af03f44f695d1e1e8f26bf
UmbraServus <ksprague0592@gmail.com>
2024-10-16 11:54:56 -0500

    Specificity calculation to help identifiy the TN rate;

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" specificity module """
+import numpy as np
+
+
+def specificity(confusion):
+    """ that calculates the specificity for each class in a confusion matrix
+        Specificity = TN / FP + TN
+        TN = Total#ofSamples - (FN + TP + FP)
+    args:
+        confusion: np.ndarr shape (classes, classes) row idxs represent the
+            corr labels & col idxs repre the pred labels
+            classes is the number of classes
+        Returns: np.ndarr of shape (classes,) containing specificity of ea class """
+    TP = np.diag(confusion)
+    FN = np.sum(confusion, axis=1) - TP
+    FP = np.sum(confusion, axis=0) - TP
+    TN = np.sum(confusion) - (FN + TP + FP)
+    specificity = TN / (FP + TN)
+    return specificity
