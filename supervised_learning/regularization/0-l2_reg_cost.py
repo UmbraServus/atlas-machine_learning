7f340ec467366347bd72bcb3fb759b2a5a688c18
UmbraServus <ksprague0592@gmail.com>
2024-10-18 01:18:12 -0500

    double space and documentation at top

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""module for L2 regularization."""
 import numpy as np
+
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """ that calculates the cost of a neural network with L2 regularization:
     args:
