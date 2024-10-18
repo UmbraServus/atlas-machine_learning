675293858196c0a351b108725781b7f0b2bb138c
UmbraServus <ksprague0592@gmail.com>
2024-10-18 01:17:35 -0500

    L2 cost. sum the square of the weights on each layer.

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import numpy as np
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """ that calculates the cost of a neural network with L2 regularization:
+    args:
+        cost: cost of the network without L2 regularization
+        lambtha: regularization parameter
+        weights: dictionary of the weights and biases (numpy.ndarrays) of the neural network
+        L: number of layers in the neural network
+        m: number of data points used
+    Returns: the cost of the network accounting for L2 regularization"""
+
+    L2 = 0
+    for i in range(1, L):
+        W = weights[f'W{i}']
+        L2 += np.sum(np.square(W))
+    T_cost = cost + (lambtha / (2 * m)) * L2
+    return T_cost
