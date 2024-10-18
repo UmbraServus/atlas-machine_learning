055d28b9b1162e9d66d505d87b4a7163650e010f
UmbraServus <ksprague0592@gmail.com>
2024-10-18 01:23:30 -0500

    forgot to account for output layer since range stops at L

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -14,7 +14,7 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Returns: the cost of the network accounting for L2 regularization"""
 
     L2 = 0
-    for i in range(1, L):
+    for i in range(1, L + 1):
         W = weights[f'W{i}']
         L2 += np.sum(np.square(W))
     T_cost = cost + (lambtha / (2 * m)) * L2
