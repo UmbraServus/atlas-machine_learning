59eea8799c2245f99f44a4b193e172ca7ec73960
UmbraServus <ksprague0592@gmail.com>
2024-10-22 15:24:44 -0500

    beggining of 0-sequential trying to resolve missing import issues atm.

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""  module for building a NN with Keras"""
+import tensorflow.keras as k
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """builds a neural network with the Keras library:
+    args:
+        nx: number of input features to the network
+        layers: list containing the # of nodes in ea layer of the network
+        activations: list containing act. functs used for ea layer of network
+        lambtha: L2 regularization parameter
+        keep_prob: probability that a node will be kept for dropout
+    You are not allowed to use the Input class
+    Returns: the keras model"""
+
+    model = k.keras.Sequential([
+
+    ])
