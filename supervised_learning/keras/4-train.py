6b27cd3f58c5e7ad5785f39005d853ed43bc8a10
UmbraServus <ksprague0592@gmail.com>
2024-10-23 11:53:33 -0500

    fixed some pycode issues im not sure what it is wanting from verbose false?!

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,22 +3,29 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs, verbose=True,
+                shuffle=False):
     """ that trains a model using mini-batch gradient descent:
     args:
         network: model to train
+
         data: np.ndarray shape (m, nx) containing the input data
+
         labels: one-hot np.ndarray shape (m, classes) containing labels
+
         batch_size: size of the batch used for mini-batch gradient descent
+
         epochs: number of passes through data for mini-batch gradient descent
         verbose: boolean determines if output shld be printed during training
+
         shuffle: boolean that determines whether to shuffle the batches
-        every epoch. Normally, it is a good idea to shuffle, but for reproducibility,
-        we have chosen to set the default to False.
+        every epoch. Normally, it is a good idea to shuffle, but for
+        reproducibility, we have chosen to set the default to False.
+
     Returns: the History object generated after training the model """
 
     model = network
-    model.fit(
+    return model.fit(
         data,
         labels,
         batch_size=batch_size,
@@ -26,4 +33,3 @@ def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle
         shuffle=shuffle,
         verbose=verbose
     )
-    return model
