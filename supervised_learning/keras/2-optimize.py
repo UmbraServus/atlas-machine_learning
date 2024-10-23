aa390df6f9ef22cb01d26b54dec2e450ec68dd5e
UmbraServus <ksprague0592@gmail.com>
2024-10-22 21:45:50 -0500

    set optimizer and compile it.

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""module for settin up adam optimization using keras """
+import tensorflow.keras as K
+
+def optimize_model(network, alpha, beta1, beta2):
+    """sets up Adam optimization for a keras model w/ categorical crossentropy
+    loss and accuracy metrics
+    args:
+        network: the model to optimize
+        alpha: learning rate
+        beta1: first Adam optimization parameter
+        beta2: second Adam optimization parameter
+    Returns: None"""
+    model = network
+    optimizer = K.optimizers.Adam(alpha, beta_1=beta1, beta_2=beta2)
+    model.compile(optimizer=optimizer, loss= 'categorical_crossentropy', metrics=['accuracy'])
