daa8843ae48ed4bbfeb0007e558789d6197e7b4d
UmbraServus <ksprague0592@gmail.com>
2024-10-23 23:35:33 -0500

    fixed 2-

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -3,18 +3,23 @@
 import numpy as np
 
 
-def convolve_grayscale_padding(images, kernel, padding): that performs a convolution on grayscale images with custom padding:
-
-images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
-m is the number of images
-h is the height in pixels of the images
-w is the width in pixels of the images
-kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
-kh is the height of the kernel
-kw is the width of the kernel
-padding is a tuple of (ph, pw)
-ph is the padding for the height of the image
-pw is the padding for the width of the image
-the image should be padded with 0’s
-You are only allowed to use two for loops; any other loops of any kind are not allowed
-Returns: a numpy.ndarray containing the convolved images
+def convolve_grayscale_padding(images, kernel, padding):
+    """performs a convolution on grayscale images with custom padding
+args:
+    images: np.ndarr w/ shape (m, h, w, c) containing multiple images
+        m is the number of images
+        h is the height in pixels of the images
+        w is the width in pixels of the images
+        c is the number of channels in the image
+    kernels: npy.ndarr w/ shape (kh, kw, c, nc) containin the kernels
+        kh is the height of a kernel
+        kw is the width of a kernel
+        nc is the number of kernels
+    padding: either a tuple of (ph, pw), ‘same’, or ‘valid’
+        if ‘same’, performs a same convolution
+        if ‘valid’, performs a valid convolution
+        if a tuple:
+            ph is the padding for the height of the image
+            pw is the padding for the width of the image
+            the image should be padded with 0’s
+Returns: a numpy.ndarray containing the convolved images"""
