a34e3b51be7e0afabc16ca1c0754b5220eea7560
UmbraServus <ksprague0592@gmail.com>
2024-10-24 22:49:42 -0500

    convole_grayscale_valid;  still a little choppy on how the equation allows the filter to move across the image. Tho I get that you are slicing the indx of the img up using the dimensions of the kernel so that must be how.

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -14,3 +14,17 @@ def convolve_grayscale_valid(images, kernel):
             kh is the height of the kernel
             kw is the width of the kernel
     Returns: np.ndarray containing the convolved images"""
+    m, input_h, input_w = images.shape
+    kernel_h, kernel_w = kernel.shape
+
+    output_h = input_h - kernel_h + 1
+    output_w = input_w - kernel_w + 1
+    output = np.zeros((m, output_h, output_w))
+
+    for x in range(output_h):
+        for y in range(output_w):
+            output[:, x, y] = np.sum(
+                images[:, x:x + kernel_h, y:y + kernel_w]
+                * kernel, axis=(1, 2)
+                )
+    return output
