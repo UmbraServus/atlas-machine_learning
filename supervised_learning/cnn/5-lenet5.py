0dbbafa53d98ca3bd7c1ef3617ae4f03492b0b3e
UmbraServus <ksprague0592@gmail.com>
2024-10-30 14:34:50 -0500

    pycode style

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -6,11 +6,11 @@ from tensorflow import keras as K
 def lenet5(X):
     """ builds a modified ver. of the LeNet-5 architecture using keras
 args:
-    X: a K.Input of shape (m, 28, 28, 1) containing the input images for the network
+    X: a K.Input of shape (m, 28, 28, 1) containing the input images
         m is the number of images
 
 The model should consist of the following layers in order:
-    
+
     Convolutional layer with 6 kernels of shape 5x5 with same padding
     Max pooling layer with kernels of shape 2x2 with 2x2 strides
     Convolutional layer with 16 kernels of shape 5x5 with valid padding
