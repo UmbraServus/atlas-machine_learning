d9a46f910d29a2db8b13464dbd3539a1c65fda0b
UmbraServus <ksprague0592@gmail.com>
2024-11-06 18:58:33 -0600

    indentions not right

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -20,36 +20,36 @@ Returns: the concatenated output of the inception block """
 
     F1, F3R, F3, F5R, F5, FPP = filters
     branch_1 = K.layers.Conv2D(filters=F1,
-                        kernel_size=1,
-                        activation='relu')(A_prev)
+                               kernel_size=1,
+                               activation='relu')(A_prev)
 
     branch_2 = K.layers.Conv2D(filters=F3R,
-                        kernel_size=1,
-                        activation='relu',
-                        padding='same')(A_prev)
+                               kernel_size=1,
+                               activation='relu',
+                               padding='same')(A_prev)
     branch_2 = K.layers.Conv2D(filters=F3,
-                        kernel_size=3,
-                        activation='relu',
+                               kernel_size=3,
+                               activation='relu',
                         padding='same')(branch_2)
 
     branch_3 = K.layers.Conv2D(filters=F5R,
-                        kernel_size=1,
-                        activation='relu',
-                        padding='same')(A_prev)
+                               kernel_size=1,
+                               activation='relu',
+                               padding='same')(A_prev)
     branch_3 = K.layers.Conv2D(filters=F5,
-                        kernel_size=5,
-                        activation='relu',
-                        padding='same')(branch_3)
+                               kernel_size=5,
+                               activation='relu',
+                               padding='same')(branch_3)
 
     branch_pool = K.layers.MaxPooling2D(pool_size=3,
-                                 strides=1,
-                                 padding='same')(A_prev)
+                                        strides=1,
+                                        padding='same')(A_prev)
     branch_pool = K.layers.Conv2D(filters=FPP,
                                   kernel_size=1,
                                   activation='relu')(branch_pool)
 
     output = K.layers.Concatenate(axis=-1)([branch_1,
-                                     branch_2,
-                                     branch_3,
-                                     branch_pool])
+                                            branch_2,
+                                            branch_3,
+                                            branch_pool])
     return output
