6a5c6f679332031eaa915881d26f342a7e781021
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2024-12-03 18:14:52 -0600

    made 0-yolo file

--- supervised_learning/transfer_learning/0-transfer.py
+++ supervised_learning/transfer_learning/0-transfer.py
@@ -2,13 +2,14 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.image import resize
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import layers, models
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from tensorflow.keras.utils import to_categorical
 
 
@@ -20,15 +21,15 @@ args:
 Returns: X_p, Y_p
     X_p is a numpy.ndarray containing the preprocessed X
     Y_p is a numpy.ndarray containing the preprocessed Y"""
-    datagen = ImageDataGenerator(
-        rescale=1.0/255.0,
-        preprocessing_function=lambda x: preprocess_input(resize(x, (224, 224), method='bilinear'))
-    )
     
-    generator = datagen.flow(X, Y)
+    # One-hot encode the labels
+    Y_p = to_categorical(Y, 10)
     
-    return generator
-
+    # Apply EfficientNet's preprocessing
+    X_p = X.astype('float32')
+    X_p = preprocess_input(X_p)
+    
+    return X_p, Y_p
 if __name__ == "__main__":
 
     #load train/valid data/test data/preprocess
@@ -41,9 +42,9 @@ if __name__ == "__main__":
     X_valid = X_train[-valid_size:]
     Y_valid = Y_train[-valid_size:]
 
-    train_gen = preprocess_data(X_trainb, Y_trainb)
-    valid_gen = preprocess_data(X_valid, Y_valid)
-    test_gen = preprocess_data(X_test, Y_test)
+    X_trainb, Y_trainb = preprocess_data(X_trainb, Y_trainb)
+    X_valid, Y_valid = preprocess_data(X_valid, Y_valid)
+    X_test, Y_test = preprocess_data(X_test, Y_test)
 
     #get pretrained model
     base_model = EfficientNetB0(
@@ -57,17 +58,19 @@ if __name__ == "__main__":
         layer.trainable = False
 
     #build new model on top of pretrain
-    inputs = layers.Input(shape=(224, 224, 3))
-    x = base_model(inputs, training=False)
+    inputs = layers.Input(shape=(32, 32, 3))
+    x = layers.Lambda(lambda img: resize(img, (224, 224)))(inputs)
+    x = base_model(x, training=False)
     gap = GlobalAveragePooling2D()(x)
-    fc1 = Dense(512, activation='relu')(gap)
-    dropout = layers.Dropout(0.5)(fc1)
-    outputs = Dense(10, activation='softmax')(dropout)
+    dropout1 = Dropout(0.5)(gap)
+    fc1 = Dense(512, activation='relu')(dropout1)
+    dropout2 = Dropout(0.5)(fc1)
+    outputs = Dense(10, activation='softmax')(dropout2)
     model = models.Model(inputs, outputs)
 
     #compile
     model.compile(
-        optimizer=Adam(1e-5),
+        optimizer=Adam(),
         loss='categorical_crossentropy',
         metrics=['accuracy'] 
         )
@@ -75,26 +78,32 @@ if __name__ == "__main__":
 
     #fit model
     history = model.fit(
-        train_gen,
-        validation_data = valid_gen,
+        X_trainb, Y_trainb,
+        validation_data = (X_valid, Y_valid),
         epochs=5,
+        batch_size=64
 
     )
     #unfreeze after initial fitting/training
-    for layer in base_model.layers[-13:]:
+    for layer in base_model.layers[:-10]:
         layer.trainable = True
 
     #recompile
     model.compile(
-        optimizer=Adam(1e-6),
+        optimizer=Adam(1e-5),
         loss='categorical_crossentropy',
         metrics=['accuracy'])
-    #continue training
+    
+    #continue training w early stopping
+    earlystop = EarlyStopping(monitor="val_loss",
+                              patience=4,
+                              restore_best_weights=True)
     history_2 = model.fit(
-        train_gen,
-        validation_data=test_gen,
-        epochs=20,
-        batch_size=128
-    )
-
-    model.save('cifar10.h5')
+        X_trainb, Y_trainb,
+        validation_data= (X_test, Y_test),
+        epochs=10,
+        batch_size=32,
+        callbacks=[earlystop]
+        )
+    model.save('cifar10.hf')
+    print("Model cifar10.h5 saved")
