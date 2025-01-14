83dcc38c2f452846df8ed64fb604f9814dd84a48
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2025-01-14 15:49:03 -0600

    My attempt at a determinant method. I had/have a hard time witht he col and rows thing. specifically how it is slicing to get the minor.

---
+++ math/advanced_linear_algebra/0-determinate.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import numpy as np
+
+def determinant(matrix):
+    """that calculates the determinant of a matrix:
+matrix: a list of lists whose determinant should be calculated
+    If matrix is not a list of lists, raise a TypeError with the message
+    matrix must be a list of lists
+    If matrix is not square, raise a ValueError with the message
+    matrix must be a square matrix
+The list [[]] represents a 0x0 matrix
+det = a*d - b*c
+Returns: the determinant of matrix"""
+
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
+    if not (isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+    
+    if len(matrix) == 0:
+        return 1
+    if len(matrix) == 1:
+        return matrix[0][0]
+    
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    
+    det = 0
+
+    for col in range(len(matrix)):
+        minor = [row[:col] + row[col + 1:] for row in matrix[1:]]
+        det += ((-1 ** col)) * matrix[0][col] * determinant(minor)
+    
+    return det
