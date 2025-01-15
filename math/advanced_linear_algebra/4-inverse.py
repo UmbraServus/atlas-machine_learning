48e15edd3dc457c208c17594bbfe029deeec31f2
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2025-01-14 21:12:24 -0600

    traling white space

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -8,9 +8,9 @@ def inverse(matrix):
     """ that calculates the inverse of a matrix
 matrix: a list of lists whose inverse should be calculated
 
-    If matrix is not a list of lists, raise a TypeError with the 
+    If matrix is not a list of lists, raise a TypeError with the
     message matrix must be a list of lists
-    If matrix is not square or is empty, raise a ValueError with the 
+    If matrix is not square or is empty, raise a ValueError with the
     message matrix must be a non-empty square matrix
 
 Returns: the inverse of matrix, or None if matrix is singular"""
