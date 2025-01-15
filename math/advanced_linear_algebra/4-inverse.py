7125ca49e2cf1067dacf24f94b5a7da7446b84ed
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2025-01-14 21:09:21 -0600

    white space

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -7,7 +7,7 @@ determinant = __import__('0-determinant').determinant
 def inverse(matrix):
     """ that calculates the inverse of a matrix
 matrix: a list of lists whose inverse should be calculated
-    
+
     If matrix is not a list of lists, raise a TypeError with the 
     message matrix must be a list of lists
     If matrix is not square or is empty, raise a ValueError with the 
