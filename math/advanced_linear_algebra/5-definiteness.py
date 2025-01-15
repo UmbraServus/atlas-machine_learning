7358ea0657f6c3633200264ca1b2b0eaab8c0f44
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2025-01-14 21:16:58 -0600

    file setup for definiteness.

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""module for calc definiteness of a matrix"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """that calculates the definiteness of a matrix:
+
+matrix: np.ndarr shape (n, n) whose definiteness should be calculated
+    If matrix is not a numpy.ndarray, raise a TypeError with the message
+    matrix must be a numpy.ndarray
+    If matrix is not a valid matrix, return None
+
+Return: the string Positive definite,
+Positive semi-definite,
+Negative semi-definite,
+Negative definite, or
+Indefinite if the matrix is positive definite,
+positive semi-definite,
+negative semi-definite,
+negative definite of indefinite, respectively
+If matrix does not fit any of the above categories, return None
+
+You may import numpy as np"""
