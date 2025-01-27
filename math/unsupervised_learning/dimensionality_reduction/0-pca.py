d0364a157a3cc8f95eb74e1df814b1b68b1fc525
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2025-01-27 02:24:43 -0600

    0-pca and readme.md;

---
+++ math/unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def pca(X, var=0.95):
+    """ that performs PCA on a dataset:
+
+X: a numpy.ndarray of shape (n, d) where:
+    n is the number of data points
+    d is the number of dimensions in each point
+    all dimensions have a mean of 0 across all data points
+var: fraction of the variance that the PCA transformation should maintain
+
+Returns: weights matrix, W, that maintains var fraction of X‘s orig variance
+    W is a numpy.ndarray of shape (d, nd) where nd is the new dimensionality
+    of the transformed X"""
+
+    n, d = X.shape
+    mean = np.mean(X, axis=0)
+    X_centered = X - mean
+    cov_matrix = np.dot(X_centered.T, X_centered) / (n - 1)
+    eigval, eigvec = np.linalg.eig(cov_matrix)
+    sorted_idxs = np.argsort(eigval)[::-1]
+    sorted_eigval = eigval[sorted_idxs]
+    sorted_eigvec = eigvec[:sorted_idxs]
+    tot_var = np.sum(sorted_eigval)
+    explained_var = np.cumsum(sorted_eigval) / tot_var
+    nd = np.argmax(explained_var >= var) + 1
+    W = sorted_eigvec[:,:nd]
+    return W
