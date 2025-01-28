48d3281a31b80385444b3f51fc9807c35b742c3a
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2025-01-28 16:23:38 -0600

    pca v2 but its just dot product of W and X_centered

---
+++ math/unsupervised_learning/dimensionality/1-pca.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""module for pca V2"""
+import numpy as np
+
+
+def pca(X, ndim):
+    """that performs PCA on a dataset:
+
+X: a numpy.ndarray of shape (n, d) where:
+    n is the number of data points
+    d is the number of dimensions in each point
+
+ndim: the new dimensionality of the transformed X
+
+Returns: T, a numpy.ndarray of shape (n, ndim) containing the transformed
+version of X"""
+
+    n, d = X.shape
+    mean = np.mean(X, axis=0)
+    X_centered = X - mean
+    cov_matrix = np.dot(X_centered.T, X_centered) / (n - 1)
+    eigval, eigvec = np.linalg.eig(cov_matrix)
+    sorted_idxs = np.argsort(eigval)[::-1]
+    sorted_eigval = eigval[sorted_idxs]
+    sorted_eigvec = eigvec[:, sorted_idxs]
+    tot_var = np.sum(sorted_eigval)
+    explained_var = np.cumsum(sorted_eigval) / tot_var
+    W = sorted_eigvec[:,:ndim]
+    T = np.dot(X_centered, W)
+    return T
