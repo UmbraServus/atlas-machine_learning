e41cdc933ffbaab30f3bc0c0bc9dc303cb4446c7
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2025-01-28 18:55:09 -0600

    I keep coming back to this but it still doesnt work .     for i in range(W.shape[1]):         if W[0, i] < 0:             W[:, i] *= -1

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -25,6 +25,7 @@ Returns: weights matrix, W, that maintains var fraction of X‘s orig variance
     explained_var = np.cumsum(sorted_eigval) / tot_var
     nd = np.argmax(explained_var >= var) + 1
     W = sorted_eigvec[:,: nd + 1]
-    if W.shape[1] == 3:
-        W[:, -1] *= -1
+    for i in range(W.shape[1]):
+        if W[0, i] < 0:
+            W[:, i] *= -1
     return W
