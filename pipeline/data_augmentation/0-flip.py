5ab6edf69677804bca64e8fd4e0925320f73d826
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-02-12 20:32:58 -0600

    fixed indents in flip.py

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -4,12 +4,10 @@ import tensorflow as tf
 
 
 def flip_image(image):
-  """ method that flips an image horizontally:
+    """method that flips an image horizontally:
 args:
     image: a 3D tf.Tensor containing the image to flip
 Returns:
     the flipped image"""
-
     flipped_image = tf.image.flip_left_right(image)
-
     return flipped_image
