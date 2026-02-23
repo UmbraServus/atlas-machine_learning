6c75f8ebf43e2e223ebf8e1fa9c185d2b5607582
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-02-22 21:45:25 -0600

    fixed pycode style in two of my files. one had no space after commas, other no end of file newline.

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -6,4 +6,4 @@ data = {
     'First': [0.0, 0.5, 1, 1.5],
     'Second': ['one', 'two', 'three', 'four']
 }
-df = pd.DataFrame(data, index=['A','B','C','D'])
+df = pd.DataFrame(data, index=['A', 'B', 'C', 'D'])
