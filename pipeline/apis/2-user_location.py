5cf7c34030340ade27add9615bd520c3f1db09b6
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-02-26 21:50:12 -0600

    getting location data from user data from a given url using sysargv

---
+++ pipeline/apis/2-user_location.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+import time
+
+import requests as r
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = r.get(url)
+
+    if response.status_code == 404:
+        print("Not Found")
+
+    elif response.status_code == 403:
+        reset_time = int(response.headers.get('X-RateLimit-Reset'))
+        current_time = int(time.time())
+        time_remaining = (reset_time - current_time) // 60
+
+        print(f"Reset in {time_remaining} min")
+
+    elif response.status_code == 200:
+        data = response.json()
+        print(data.get('location'))
+
+    else:
+        print("Not Found")
