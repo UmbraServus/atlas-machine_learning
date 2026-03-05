a3d180b7b9b44b138135a436e89f98b850bc0869
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-03-04 20:57:51 -0600

    restrictions on user table using unique and not null 13-; added country column enum to keep it predetermined; ranks country origins of bands by n of fans 15-.

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,7 @@
+-- creates a user table with restrictions.
+CREATE TABLE IF NOT EXISTS users (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(255) NOT NULL UNIQUE,
+name VARCHAR(255),
+country ENUM('US', 'CO','TN') NOT NULL DEFAULT 'US'
+);
