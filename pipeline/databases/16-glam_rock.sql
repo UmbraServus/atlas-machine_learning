3ea5dd7ed493e83f36c2f581130007c57177390a
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-03-04 21:12:50 -0600

    That wasnt it and I cant get the source to download so I am not sure of the structure of the data.

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,7 +1,5 @@
 -- lists all Glam rock bands ranked by longevity
-SELECT
-    band_name,
-    (IFNULL(CAST(split AS SIGNED), 2020) - CAST(formed AS SIGNED)) AS lifespan
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
