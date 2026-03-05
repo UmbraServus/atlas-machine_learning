22240d2605327971a5b2974552fc5d5f216d0968
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-03-05 12:20:00 -0600

    18-valid_email changes the validation of email to default if email is changed from old email.

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,13 @@
+-- trigger that resets the attribute valid_email when email has been changed.
+DELIMITER $$
+
+CREATE TRIGGER reset_attribute
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET  NEW.valid_email = 0;
+    END IF;
+END $$
+
+DELIMITER ;
