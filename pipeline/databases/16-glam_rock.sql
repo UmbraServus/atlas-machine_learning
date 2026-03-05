5c254802d4079a8a730ae1fa82f4cae04513f180
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-03-04 21:17:26 -0600

    trying one last thing before I move on. maybe its the way I am trying to access style?

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,8 @@
 -- lists all Glam rock bands ranked by longevity
 SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
-FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
+FROM
+    metal_bands
+WHERE
+    LOWER(style) LIKE '%glam rock%'
+ORDER BY
+    lifespan DESC;
