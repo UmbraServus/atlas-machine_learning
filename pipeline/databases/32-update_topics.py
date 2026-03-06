00559c3d6295a24f585c21d9f3ac2859defc6bd2
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-03-06 05:25:09 -0600

    32-update_topics.py - updates the attribute topics given school name.

---
+++ pipeline/databases/32-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""module for updating topics attribute in a collection of documents"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics attribute in a collection of documents
+    args:
+    mongo_collection: pymongo collection object
+    name (string): school name to update
+    topics (list of strings): list of topics approached in the school"""
+
+    mongo_collection.update_one(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
