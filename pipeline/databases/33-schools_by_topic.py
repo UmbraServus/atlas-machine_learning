d53db289869ff6b74e8632fff553142f8092da8f
UmbraServus <155484678+UmbraServus@users.noreply.github.com>
2026-03-06 05:35:12 -0600

    33-schools_by_topic.py - similar to 30-all.py but specific to a certain attribute. (topics in this case)

---
+++ pipeline/databases/33-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""method for listing all documents in a collection in mongodb"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ method for listing all documents in a collection in mongodb
+args:
+    mongo_collection: the pymongo collection object
+    topic (string): topic searched
+returns:
+    list: documents in a collection in mongodb where topic is specified"""
+    if mongo_collection or topic is None:
+        return []
+    else:
+        return list(mongo_collection.find({"topics": topic}))
